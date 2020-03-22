88315f572c13840584d003f1c7987118214da0ec
Kyle Pearson <pearsonk@email.arizona.edu>
2020-03-22 15:22:01 -0700

    v1

---
+++ Assets/CaptureBackground.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptureBackground : MonoBehaviour
+{
+    [SerializeField] Material mat = null;
+
+    Texture2D background;
+    WebCamTexture webcam;
+    public RawImage rawimage;
+
+
+    void Start()
+    {
+        // Init camera
+        webcam = new WebCamTexture(640, 480, 30);
+        rawimage.texture = webcam;
+        webcam.Play();
+
+        transform.localScale = new Vector3(webcam.width / 100f, 1, webcam.height / 100f);
+
+        background = new Texture2D(webcam.width, webcam.height, TextureFormat.ARGB32, false);
+        Background();
+    }
+
+    public void Background()
+    {
+        var pixels = webcam.GetPixels(0, 0, webcam.width, webcam.height);
+        background.SetPixels(pixels);
+        background.Apply();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetTexture("webcam", webcam);
+        mat.SetTexture("background", background);
+    }
+
+    void OnDestroy()
+    {
+        webcam?.Stop();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Difference-Matte-Unity
 A real time difference matte effect for the webcam built with Unity Shader graph. Create an artificial green screen in real time by subtracting the background
+
+![](difference_matte.gif)
+
+If you can disable the auto-exposure on your webcam (e.g. color-balance, exposure time, gamma, etc) the background subtraction will have a much higher quality otherwise you will see some residual error.
